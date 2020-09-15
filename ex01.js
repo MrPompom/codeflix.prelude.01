63952a8ed10f5d29f7b7936f9403d83499ea2fb6
bastien prompsy <bastien.prompsy@efrei.net>
2020-09-15 11:43:52 +0200

    delete ex02 + test

--- ex01.js
+++ ex01.js
@@ -1,12 +1,16 @@
 function lowercase(str)
 {
-    var test[];
+    var test = [];
     for (var i = 0; i < str.length;i += 1) 
     {
-        if (str[i] < 'Z' && str[i] > 'A')
-        test[i] = str[i].charCodeat(0) - 32;
-        else
-        test[i] = str[i].charCodeat(0);
+        if (str[i] < 'Z' && str[i] > 'A') {
+        console.log("1");
+        test[i] = (str[i].charCodeAt(0) + 32);
+    }
+        else {
+            test[i] = str[i].charCodeAt(0);
+            console.log("2");
+        }
 
         test = String.fromCharCode(...test);
     }

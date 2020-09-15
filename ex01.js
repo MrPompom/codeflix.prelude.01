74430191a207c12065da320621483cf2a7d13b4f
bastien prompsy <bastienprompsy@gmail.com>
2020-09-15 11:45:22 +0200

    upgrade ex01

--- ex01.js
+++ ex01.js
@@ -1,21 +1,21 @@
-function lowercase(str)
+function lowerCase(str)
 {
-    var test = [];
-    for (var i = 0; i < str.length;i += 1) 
-    {
-        if (str[i] < 'Z' && str[i] > 'A') {
-        console.log("1");
-        test[i] = (str[i].charCodeAt(0) + 32);
+  // string.charCodeAt()
+  var test = [];
+  for (var i = 0; i < str.length; i += 1) 
+  {
+    if (str[i] < 'z' && str[i] > 'a') {
+      test[i] = (str[i].charCodeAt(0) - 32);
     }
-        else {
-            test[i] = str[i].charCodeAt(0);
-            console.log("2");
-        }
+    else
+    test[i] = str[i].charCodeAt(0);
+  }
+   test = String.fromCharCode(...test);
+   
 
-        test = String.fromCharCode(...test);
-    }
-    console.log(test);
+  console.log(test);
 }
 
-lowercase('Chopper');
-lowercase('TONY TONY');
+lowerCase('Chopper');
+
+lowerCase('tony tony');
